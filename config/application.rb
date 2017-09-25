02d9f2158a6dd67061cedfde69c6c03a4ff3c752
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-09-25 10:38:02 -0600

    Changes

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# See https://codereview.stackexchange.com/questions/144435/set-env-variables-for-ruby-on-rails :
-# SECRETS = Rails.application.secrets
-
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -17,5 +14,11 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+	# See https://codereview.stackexchange.com/questions/144435/set-env-variables-for-ruby-on-rails :
+	# SECRETS = Rails.application.secrets
+	# ENV[:database_password] = Rails.application.secrets[:database_password]
+	ENV['DATABASE_USERNAME'] = Rails.application.secrets[:database_username]
+	ENV['DATABASE_PASSWORD'] = Rails.application.secrets[:database_password]
   end
 end
