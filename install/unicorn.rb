a5c3641bceefc7146abe3066a4005a9ee89cf2fc
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-09-24 18:31:23 -0600

    Fixed the paths in unicorn.rb

--- install/unicorn.rb
+++ install/unicorn.rb
@@ -1,12 +1,16 @@
 # From https://www.linode.com/docs/development/ror/use-unicorn-and-nginx-on-ubuntu-14-04 :
 
 # Set the path to the application:
+base_dir = "/var/www/apps/rails-stack-example"
 # app_dir git File.expand_path("../..", __FILE__)
 # app_dir = "/var/www/apps/rails-stack-example"
-app_dir = "/var/www/apps/rails-stack-example/current"
-log_dir = "#{app_dir}/log"
+app_dir = "#{base_dir}/current"
+shared_dir = "#{base_dir}/shared"
+
+log_dir = "#{shared_dir}/log"
 # tmp_dir = "#{app_dir}/tmp"
-tmp_dir = "#{app_dir}/shared/tmp"
+tmp_dir = "#{shared_dir}/tmp"
+
 working_directory app_dir
 
 # Set the Unicorn options:
@@ -14,12 +18,12 @@ worker_processes 2
 preload_app true
 timeout 30
 
-# Set the path to the Unicorn socket:
-listen "#{tmp_dir}/sockets/unicorn.sock", :backlog => 64
-
 # Set the paths for logging:
 stderr_path "#{log_dir}/unicorn.stderr.log"
 stdout_path "#{log_dir}/unicorn.stdout.log"
 
 # Set the path to the process ID file:
 pid "#{tmp_dir}/pids/unicorn.pid"
+
+# Set the path to the Unicorn socket:
+listen "#{tmp_dir}/sockets/unicorn.sock", :backlog => 64
