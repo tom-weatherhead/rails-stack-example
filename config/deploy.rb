9580aaa0e54e132869dca73443bdc8cc3724b15e
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-09-25 11:00:41 -0600

    Confifugre Capistrano to link to config/secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set :deploy_to, "/var/www/apps/rails-stack-example"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/secrets.yml"
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
